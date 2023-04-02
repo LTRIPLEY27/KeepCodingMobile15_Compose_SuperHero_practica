3399c172786c14185261d5ab22da596f5e0f4720
IsaDevs <isabel.calzadilla.18@gmail.com>
2023-04-02 13:26:51 +0200

    Initial Commit

---
+++ project_superheroesMarvel/project_superheroesMarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_superheroesMarvelApp.swift
+//  project_superheroesMarvel
+//
+//  Created by IsaDevs on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct project_superheroesMarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
